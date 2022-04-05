4803036c28f3009400bdb02cacb34bfd4381986b
Aditya Jain <vvasujain@gmail.com>
2022-04-06 02:39:40 +0530

    created contract with signup flow and wrote tests for it

--- contract/contracts/Dtwitter.sol
+++ contract/contracts/Dtwitter.sol
@@ -4,17 +4,34 @@ pragma solidity ^0.8.4;
 import "hardhat/console.sol";
 
 contract Dtwitter {
-    string public name = "Aditya Jain";
+    struct User {
+        address wallet;
+        string name;
+        string handle;
+        string bio;
+        string avatar;
+    }
 
-    // constructor (string memory _name) public {
-    //     name = _name;
-    // }
+    mapping(address => string) public handles;
+    mapping(string => User) public users;
 
-    // function getName() public view returns(string memory) {
-    //     return name;
-    // }
+    function signUp(string memory _handle, string memory _name, string memory _bio, string memory _avatar) public {
+        
+        require(bytes(handles[msg.sender]).length == 0, "User already exists!");
+        require(users[_handle].wallet == address(0), "Username is already taken!");
 
-    function setName(string memory _name) public {
-        name = _name;
+        users[_handle] = User({
+            wallet: msg.sender,
+            name: _name,
+            handle: _handle,
+            bio: _bio,
+            avatar: _avatar
+        });
+
+        handles[msg.sender] = _handle;
+    }
+
+    function getUserfromWallet(address _wallet) public view returns (User memory) {
+        return users[handles[_wallet]];
     }
 }
--- contract/test/dtwitter-test.js
+++ contract/test/dtwitter-test.js
@@ -2,18 +2,37 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Dtwitter", function () {
-  it("Should return the name", async function () {
+  it("Testing signup flow", async function () {
     const Dtwitter = await ethers.getContractFactory("Dtwitter");
     const dtwitter = await Dtwitter.deploy();
+    const [user1, user2] = await ethers.getSigners();
     await dtwitter.deployed();
 
-    expect(await dtwitter.name()).to.equal("Aditya Jain");
+    await dtwitter.signUp("adi_9056", "adi", "I'm awesome", "some.gif");
+    console.log("Signing Up User");
 
-    const setName = await dtwitter.setName("Aditya Jain");
+    const user = await dtwitter.users("adi_9056");
+    expect(user.name).to.equal("adi");
+    expect(user.bio).to.equal("I'm awesome");
+    expect(user.avatar).to.equal("some.gif");
+    console.log("Test signup for adi")
 
-    // wait until the transaction is mined
-    await setName.wait();
+    expect(await dtwitter.handles(user1.address)).to.equal("adi_9056");
 
-    expect(await dtwitter.name()).to.equal("Aditya Jain");
+    await expect(dtwitter.signUp("", "", "", "")).to.be.revertedWith(
+      "User already exists!"
+    );
+
+    await expect(
+      dtwitter.connect(user2).signUp("adi_9056", "adi", "I'm awesome", "some.gif")).to.be.revertedWith(
+      "Username is already taken!"
+    );
+
+    // testing getUserfromWallet
+    const userFromWallet = await dtwitter.getUserfromWallet(user1.address);
+    expect(userFromWallet.name).to.equal("adi");
+    expect(userFromWallet.bio).to.equal("I'm awesome");
+    expect(userFromWallet.avatar).to.equal("some.gif");
+    console.log("Test getUserfromWallet function")
   });
 });
